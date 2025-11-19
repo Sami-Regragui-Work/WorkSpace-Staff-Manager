4940d2d4c2d7431c2560c1289942d5ebeb427f46
SamiReg <samireg2002@gmail.com>
2025-11-19 12:50:24 +0100

    feat: wokers add and edit

--- assets/js/sideBarCollapse.js
+++ assets/js/sideBarCollapse.js
@@ -2,33 +2,14 @@ const sidebarBtn = document.getElementById("sidebar__btn");
 const sidebarArrow = document.getElementById("aside__arrow");
 const sideBar = document.getElementById("sidebar");
 
-console.log(sidebarArrow.getBoundingClientRect());
-
-let collapsed = -1;
+// console.log(sidebarArrow.getBoundingClientRect());
 
 sidebarBtn.addEventListener("click", () => {
-    // arrowBtn.style.transition = "rotate 500ms ease-in-out";
     sidebarArrow.firstElementChild.classList.toggle("rotate-180");
     Array.from(sideBar.children).forEach((node, ind) => {
-        // console/.log(node.id);
-        // if (ind == 0) {
-        //     // node.children[0].classList.toggle("w-0");
-        //     node.children[0].innerHTML = collapsed < 0 ? "" : "Staff Members";
-        // } else if (node.id !== "sidebar__btn") node.classList.toggle("w-0");
-
-        // if (ind == 2) {
-        //     node.innerHTML = collapsed < 0 ? "" : "Add New Worker";
-        //     // console.log
-        // }
-        // collapsed *= -1;
         if (ind == 0) {
-            // node.children[0].classList.toggle("w-0");
             node.children[0].classList.toggle("hidden");
         } else if (node.id !== "sidebar__btn") node.classList.toggle("hidden");
     });
     sideBar.classList.toggle("flex-1");
-    // sidebarBtn.classList.toggle("absolute");
-    // sideBar.classList.toggle("w-full");
-    // sideBar.classList.toggle("w-fit");
-    console.log(sidebarArrow.getBoundingClientRect());
 });
--- assets/js/workersCRUD.js
+++ assets/js/workersCRUD.js
@@ -1,80 +1,250 @@
 const addWorkerBtn = document.getElementById("add-worker");
+const addExperienceBtn = document.getElementById("add-experience");
+const saveWorkerBtn = document.getElementById("save-worker");
 const workersDiv = document.getElementById("aside-workers");
+const modal = document.getElementById("crud-modal");
+const modalForum = document.getElementById("add-worker-modal");
+const experiences = document.getElementById("experiences");
+const picUrl = document.getElementById("pic");
+const closeModal = document.querySelectorAll(".close__modal");
 
-// worker example
-// {"id":1, ""}
+// to reset experiences to initial state (one experiences)
+const experiencesInitialState = experiences.innerHTML;
 
-{
-    /* 
+const workers = [];
 
-<!-- Modal toggle -->
-<button data-modal-target="crud-modal" data-modal-toggle="crud-modal" class="text-white bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none" type="button">
-  Toggle modal
-</button>
+let expCounter;
+let GlobalId = 0;
 
-<!-- Main modal -->
-<div id="crud-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div class="relative p-4 w-full max-w-md max-h-full">
-        <!-- Modal content -->
-        <div class="relative bg-neutral-primary-soft border border-default rounded-base shadow-sm p-4 md:p-6">
-            <!-- Modal header -->
-            <div class="flex items-center justify-between border-b border-default pb-4 md:pb-5">
-                <h3 class="text-lg font-medium text-heading">
-                    Create new product
-                </h3>
-                <button type="button" class="text-body bg-transparent hover:bg-neutral-tertiary hover:text-heading rounded-base text-sm w-9 h-9 ms-auto inline-flex justify-center items-center" data-modal-hide="crud-modal">
-                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6"/></svg>
-                    <span class="sr-only">Close modal</span>
-                </button>
-            </div>
-            <!-- Modal body -->
-            <form action="#">
-                <div class="grid gap-4 grid-cols-2 py-4 md:py-6">
-                    <div class="col-span-2">
-                        <label for="name" class="block mb-2.5 text-sm font-medium text-heading">Name</label>
-                        <input type="text" name="name" id="name" class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" placeholder="Type product name" required="">
-                    </div>
-                    <div class="col-span-2 sm:col-span-1">
-                        <label for="price" class="block mb-2.5 text-sm font-medium text-heading">Price</label>
-                        <input type="number" name="price" id="price" class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" placeholder="$2999" required="">
-                    </div>
-                    <div class="col-span-2 sm:col-span-1">
-                        <label for="category" class="block mb-2.5 text-sm font-medium text-heading">Category</label>
-                        <select id="category" class="block w-full px-3 py-2.5 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand px-3 py-2.5 shadow-xs placeholder:text-body">
-                            <option selected="">Select category</option>
-                            <option value="TV">TV/Monitors</option>
-                            <option value="PC">PC</option>
-                            <option value="GA">Gaming/Console</option>
-                            <option value="PH">Phones</option>
-                        </select>
-                    </div>
-                    <div class="col-span-2">
-                        <label for="description" class="block mb-2.5 text-sm font-medium text-heading">Product Description</label>
-                        <textarea id="description" rows="4" class="block bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full p-3.5 shadow-xs placeholder:text-body" placeholder="Write product description here"></textarea>                    
-                    </div>
-                </div>
-                <div class="flex items-center space-x-4 border-t border-default pt-4 md:pt-6">
-                    <button type="submit" class="inline-flex items-center  text-white bg-brand hover:bg-brand-strong box-border border border-transparent focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">
-                        <svg class="w-4 h-4 me-1.5 -ms-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5"/></svg>
-                        Add new product
-                    </button>
-                    <button data-modal-hide="crud-modal" type="button" class="text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">Cancel</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div> 
- */
+const fieldsLabs = [
+    "name",
+    "role",
+    "email",
+    "phone",
+    "photoUrl",
+    "experiences",
+];
+
+const expLabs = ["company", "role", "from", "to"];
+
+// async function fetchJSON(callback, ...args) {
+//     try {
+//         const res = (await fetch("assets/workers.json")) || [];
+//         if (!res.ok) throw new Error("Failed to fetch JSON");
+//         const resBody = await res.json();
+//         callback(...args, resBody);
+//     } catch (err) {
+//         console.error("Error:", err);
+//     }
+// }
+
+function getWorkersIndex(id) {
+    return workers.findIndex((worker) => worker.id == id);
 }
 
-function openModal() {}
+function addWorker(e, edit = false) {
+    expCounter = 1;
+    if (!edit) {
+        experiences.innerHTML = experiencesInitialState;
+        // console/.log("reseted");
+        modalForum.reset();
+        modalForum.querySelector("input[type='hidden']").value = "";
+        setPreview();
+        modal.classList.toggle("hidden");
+        modal.classList.toggle("flex");
+        modal.ariaHidden = false;
+    } else {
+        const clickTarget = e.target;
+        if (clickTarget.tagName == "BUTTON") {
+            id = clickTarget.parentElement.getAttribute("worker-id");
+            const infos = workers.filter((infos) => infos.id == id)[0];
+            // expCounter = infos.experiences.length;
+            setWorkerInfos(infos);
+            modal.classList.toggle("hidden");
+            modal.classList.toggle("flex");
+            modal.ariaHidden = false;
+            // console/.log("edit mode");
+        }
+    }
+}
 
-function addWorker() {
-    openModal();
+function showAddedWorker(workerInfos, edit = false) {
+    if (!edit) {
+        console.log("add mode");
+        const roleText =
+            modalForum.querySelector("#role").options[workerInfos.role].text;
+        const divTemplate = `
+        <div worker-id=${workerInfos.id} class='flex gap-3 items-center bg-[color-mix(in_oklab,var(--accent-clr)_10%,transparent_90%)] p-(--padding-g) rounded-(--b-r) '>
+            <div class='aside__worker__left flex gap-3 items-center '>
+                <img class='img img--sidebar min-h-[6rem] max-h-[6rem]' src=${workerInfos.photoUrl}>
+                <div class='flex flex-col'>
+                    <h3>${workerInfos.name}</h3>
+                    <span class='text-(--accent-clr)' style='font-size: var(--fs-text)'>${roleText}</span>
+                </div>
+            </div>
+            <button class='btn text-(--secondary-clr) ml-auto' style='font-size: var(--fs-text)' >Edit</button>
+        </div>
+    `;
+        workersDiv.innerHTML += divTemplate;
+    } else {
+        console.log("edit mode");
+        const div = document.querySelector(`[worker-id='${workerInfos.id}']`);
+        const roleText =
+            modalForum.querySelector("#role").options[workerInfos.role].text;
+        div.innerHTML = `
+        <div class='aside__worker__left flex gap-3 items-center '>
+                <img class='img img--sidebar min-h-[6rem] max-h-[6rem]' src=${workerInfos.photoUrl}>
+                <div class='flex flex-col'>
+                    <h3>${workerInfos.name}</h3>
+                    <span class='text-(--accent-clr)' style='font-size: var(--fs-text)'>${roleText}</span>
+                </div>
+            </div>
+            <button class='btn text-(--secondary-clr) ml-auto' style='font-size: var(--fs-text)' >Edit</button>
+    `;
+    }
+}
+
+function storeWorkerInfos(values) {
+    // infos object visualisation
+    // const infos = {
+    //     id: ++GlobalId,
+    //     name: "",
+    //     role: "",
+    //     email: "",
+    //     phone: "",
+    //     photoUrl: "",
+    //     experiences: [{
+    //         company: "",
+    //         role: "",
+    //         from: "",
+    //         to: ""
+    //     }]
+    // };
+    console.log("id input:", values[0]);
+    edit = Boolean(values[0]);
+    const infos = { id: edit ? values[0] : ++GlobalId };
+
+    values.slice(1).forEach((value, ind) => {
+        if (ind < 5) {
+            infos[fieldsLabs[ind]] = value;
+
+            // console/.log(infos);
+        } else {
+            const expInd = Math.floor((ind - 5) / 4);
+            const expFieldInd = (ind - 5) % 4;
+            // console/.log(expFieldInd);
+            if (!expFieldInd) {
+                // console/.log("expInd " + expInd);
+                // console/.log("expFieldInd " + expFieldInd);
+                // console/.log(infos);
+                if (!expInd) {
+                    // console/.log(infos);
+                    // console/.log(infos.experiences);
+                    infos.experiences = [{}];
+                } else infos.experiences.push({});
+            }
+            infos.experiences[expInd][expLabs[expFieldInd]] = value;
+        }
+    });
+
+    // fetch("assets/workers.json").then((res) => )
+    // console/.log(infos);
+
+    if (edit) {
+        workers.splice(getWorkersIndex(infos.id), 1, infos);
+    } else workers.push(infos);
+    // console/.log(infos);
+    showAddedWorker(infos, edit);
+}
+
+function setWorkerInfos(infos) {
+    experiences.innerHTML = experiencesInitialState;
+    const inputs = Array.from(modalForum.querySelectorAll("div>div>input"));
+    const dropDown = modalForum.querySelector("#role");
+    const expInputs = Array.from(experiences.querySelectorAll("input"));
+    inputs.splice(2, 0, dropDown);
+    let prevExpInd = 0;
+
+    Object.values(infos).forEach((value, ind) => {
+        // console/.log(value);
+        if (ind < 6) {
+            inputs[ind].value = value;
+
+            // console/.log(infos);
+        } else {
+            const exps = value; // array
+            exps.forEach((row, expInd) => {
+                if (expInd > prevExpInd) {
+                    prevExpInd = expInd;
+                    addExperience();
+                    expInputs.push(
+                        ...Array.from(
+                            experiences.querySelectorAll(
+                                `#experience_${expInd + 1} input`
+                            )
+                        )
+                    );
+                    // console/.log(
+                    //     experiences.querySelectorAll(
+                    //         `#experience_${expInd + 1} input`
+                    //     )
+                    // );
+                }
+                Object.values(row).forEach((expValue, expFieldInd) => {
+                    // console/.log(expInputs);
+                    expInputs[expInd * 4 + expFieldInd].value = expValue;
+                    // console/.log(expInputs[expInd * 4 + expFieldInd].value);
+                });
+            });
+        }
+    });
+}
+
+function getWorkerInfos(e) {
+    e.preventDefault();
+    // console/.log(modalForum);
+    const inputs = modalForum.querySelectorAll("input");
+    const dropDown = modalForum.querySelector("#role");
+
+    let values = Array.from(inputs).map((input) => input.value);
+    const roleVal = dropDown.value;
+    values.splice(2, 0, roleVal);
+    // console/.log(values);
+    storeWorkerInfos(values);
+}
+
+function setPreview() {
+    picUrl.parentElement.nextElementSibling.firstElementChild.src =
+        picUrl.value;
+}
+
+function addExperience() {
+    // console/.log("cc");
+    // e.preventDefault();
+    const newExp = experiences.firstElementChild.cloneNode(true);
+    newExp.querySelectorAll("input").forEach((input) => (input.value = ""));
+    newExp.setAttribute("id", `experience_${++expCounter}`);
+    // const addBeforeThis = addExperienceBtn.parentNode;
+
+    experiences.appendChild(newExp);
 }
 
 function initialize() {
     addWorkerBtn.addEventListener("click", addWorker);
+    saveWorkerBtn.addEventListener("click", getWorkerInfos);
+    addExperienceBtn.addEventListener("click", addExperience);
+    picUrl.addEventListener("input", setPreview);
+    workersDiv.addEventListener("click", (e) => {
+        addWorker(e, true);
+    });
+    closeModal.forEach((btn) => {
+        btn.addEventListener("click", (e) => {
+            modal.classList.toggle("hidden");
+            modal.classList.toggle("flex");
+            modal.ariaHidden = true;
+            e.target.blur();
+        });
+    });
 }
 
 initialize();
